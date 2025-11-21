eef6fef5313e6ae7b9c4107121db1e07c59faf75
Gaurav Vaishnav <gaurav.v@netizens.email>
2025-11-21 14:16:12 +0530

    composer pint

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -14,9 +14,9 @@ use Filament\Tables\TablesServiceProvider;
 use Filament\Widgets\WidgetsServiceProvider;
 use Illuminate\Database\Eloquent\Factories\Factory;
 use Livewire\LivewireServiceProvider;
+use Netizensgaurav\FilamentOnboarding\FilamentOnboardingServiceProvider;
 use Orchestra\Testbench\TestCase as Orchestra;
 use RyanChandler\BladeCaptureDirective\BladeCaptureDirectiveServiceProvider;
-use Netizensgaurav\FilamentOnboarding\FilamentOnboardingServiceProvider;
 
 class TestCase extends Orchestra
 {
